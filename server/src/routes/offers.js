8578ceba04adc8aeca35c13dd23ac2e2fb47d9dc
Teemu Harinen <teemuharinen1@gmail.com>
2024-09-24 17:18:08 +0300

    add backend support for new offer creation

--- server/src/routes/offers.js
+++ server/src/routes/offers.js
@@ -2,12 +2,13 @@ import express from "express";
 import Offer from "../models/Offer.js";
 const router = express.Router();
 
+// Fetch offers by productName and county
 router.post('/', async (req, res, next) => {
   const { productName, county } = req.body;
   
   try {
     const offers = await Offer.find({ productName, county });
-    console.log(offers);
+
     if (offers.length === 0) {
       return res.status(404).json({ message: 'No offers found' });
     }
@@ -15,6 +16,19 @@ router.post('/', async (req, res, next) => {
     return res.status(200).json(offers);
   } catch (error) {
     res.status(500).json({ message: 'Error finding offers', error });
+  }
+});
+
+// Create a new offer
+router.post('/create', async (req, res, next) => {
+  const { productName, amount, price, county, city, streetAddress, zipCode, description } = req.body;
+  const newOffer = new Offer({ productName, amount, price, county, city, streetAddress, zipCode, description });
+
+  try {
+    const offer = await newOffer.save();
+    res.status(201).json(offer);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating offer', error });
   }
 });
 
