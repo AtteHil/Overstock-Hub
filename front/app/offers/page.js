04a8e9ce9d58a7cab8cf092128123372f4855845
AleksiLe <aleksi.levanen@gmail.com>
2024-09-25 12:26:25 +0300

    Styling on offers page

--- front/app/offers/page.js
+++ front/app/offers/page.js
@@ -16,43 +16,79 @@ export default function Offers(){
     }
     
     return(
-        <Grid2 container spacing={2}>
-            <Grid2 sx={{mt: 12}} size={4}>{offers}</Grid2>
-            <Grid2 sx={{mt: 12}} size={4}>
-            <Autocomplete
-            id="county-select"
-            sx={{ width: 300 }}
-            options={finnishCountyList}
-            autoHighlight
-            getOptionLabel={(option) => option}
-            renderOption={(props, option) => {
-                const { key, ...optionProps } = props;
-                return (
-                <Box
-                    key={key}
-                    component="li"
-                    sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-                    {...optionProps}
-                >
-                    
-                    {option}
-                </Box>
-                );
-            }}
-            renderInput={(params) => (
-                <TextField
-                {...params}
-                label="Choose a county"
-                slotProps={{
-                    htmlInput: {
-                    ...params.inputProps,
-                    autoComplete: 'new-password', // disable autocomplete and autofill
-                    },
-                }}
-                />
-            )}
-            
-    /></Grid2></Grid2>
+        <Grid2 container spacing={0}>
+            <Grid2 sx={{ mt: 12 }} size={6}>{offers}</Grid2>
+            <Grid2 sx={{ mt: 12,
+                position: "fixed",   // Keeps it fixed in the viewport
+                top: "20px",         // Distance from the top of the page
+                right: "20px",      // Distance from the right side of the page
+                zIndex: 5,        // Ensure it's on top of other elements
+                }} size={5}>    
+                <Autocomplete
+                    id="county-select"
+                    sx={{
+                        width: 300,
+                        "& .MuiOutlinedInput-root": {
+                            backgroundColor: "#f0f0f0", // Background color of input
+                            "&:hover": {
+                                backgroundColor: "#e0e0e0", // Hover color of input
+                            },
+                            "&.Mui-focused fieldset": {
+                                borderColor: "#1976d2", // Border color when focused
+                            },
+                        },
+                        // Styles for the dropdown menu
+                        "& .MuiAutocomplete-paper": {
+                            backgroundColor: "#fafafa", // Background color of the dropdown
+                            color: "#000", // Text color in the dropdown
+                        },
+                        // Styles for individual dropdown options
+                        "& .MuiAutocomplete-option": {
+                            color: "#000", // Text color of dropdown options
+                            "&[aria-selected='true']": {
+                                backgroundColor: "#cfe8fc", // Background color of selected option
+                            },
+                            "&:hover": {
+                                backgroundColor: "#e3f2fd", // Hover background color for options
+                            },
+                        },
+                        // Styles for the popper (autocomplete container)
+                        "& .MuiAutocomplete-popper": {
+                            boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)", // Custom shadow for dropdown
+                        },
+                    }}
+
+                    options={finnishCountyList}
+                    autoHighlight
+                    getOptionLabel={(option) => option}
+                    renderOption={(props, option) => {
+                        const { key, ...optionProps } = props;
+                        return (
+                            <Box
+                                key={key}
+                                component="li"
+                                sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+                                {...optionProps}
+                            >
+
+                                {option}
+                            </Box>
+                        );
+                    }}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Choose a county"
+                            slotProps={{
+                                htmlInput: {
+                                    ...params.inputProps,
+                                    autoComplete: 'new-password', // disable autocomplete and autofill
+                                },
+                            }}
+                        />
+                    )}
+
+                /></Grid2></Grid2>
         
         )
 };
